8bda18ac85f51bf3c413a1b8c2fac7e6ea82a638
kevindu52defargoth <>
2021-06-08 15:59:55 +0200

    finalement le compteur

--- dec7seg.vhd
+++ dec7seg.vhd
@@ -9,25 +9,50 @@ END dec7seg;
 ARCHITECTURE numero1 OF dec7seg IS
 BEGIN
 process(unite)
+Begin
 	--unité
 	CASE unite IS
-	WHEN 0 =>
-		s(1) <= '1';
-		s(2) <= '1';
-		s(3) <= '1';
-		s(4) <= '1';
-		s(5) <= '1';
-		s(6) <= '0';
-
-	WHEN 1 =>
-		s(1) <= '0';
-		s(2) <= '1';
-		s(3) <= '1';
-		s(4) <= '0';
-		s(5) <= '0';
-		s(6) <= '0';
-
+	 when 0=> segments_unite <= "0000001";   
+    when 1=> segments_unite <= "1001111";
+    when 2=> segments_unite <= "0010010";
+    when 3=> segments_unite <= "0000110";
+    when 4=> segments_unite <= "1001100";
+    when 5=> segments_unite <= "0100100";
+    when 6=> segments_unite <= "0100000";
+    when 7=> segments_unite <= "0001111";
+    when 8=> segments_unite <= "0000000";   
+    when 9=> segments_unite <= "0000100";
 	END CASE;
+	
+	--dizaine
+	CASE dizaine is
+	when 0=> segments_dizaine <= "0000001";  
+	when 1=> segments_dizaine <= "1001111";
+	when 2=> segments_dizaine <= "0010010";
+	when 3=> segments_dizaine <= "0000110";
+	when 4=> segments_dizaine <= "1001100";
+	when 5=> segments_dizaine <= "0100100";
+	when 6=> segments_dizaine <= "0100000";
+	when 7=> segments_dizaine <= "0001111";
+	when 8=> segments_dizaine <= "0000000";  
+	when 9=> segments_dizaine <= "0000100";
+	END CASE;
+
+	--centaine
+	CASE centaine is
+	when 0=> segments_centaine <= "0000001";  
+	when 1=> segments_centaine <= "1001111";
+	when 2=> segments_centaine <= "0010010";
+	when 3=> segments_centaine <= "0000110";
+	when 4=> segments_centaine <= "1001100";
+	when 5=> segments_centaine <= "0100100";
+	when 6=> segments_centaine <= "0100000";
+	when 7=> segments_centaine <= "0001111";
+	when 8=> segments_centaine <= "0000000";  
+	when 9=> segments_centaine <= "0000100";
+	END CASE;
+
+
 END process;
 	
 END numero1;
